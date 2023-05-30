e85b9aa5603051546787595e3474f9794ed75fd5
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-30 12:11:48 +1200

    zsh: aliases

--- home/common/shell/default.nix
+++ home/common/shell/default.nix
@@ -10,10 +10,26 @@
 
         initExtra = ''
             bindkey '^ ' autosuggest-accept
+
+            export EDITOR="nvim";
         '';
 
         shellAliases = {
             tmux = "tmux -f $HOME/.config/tmux/tmux.conf";
+            nrs = "sudo nixos-rebuild switch";
+            nr = "sudo nixos-rebuild";
+
+            exa = "exa -1lag -s Name --group-directories-first --icons --color=always";
+            mkdir = "mkdir -p";
+            mv = "mv -i";
+
+            gs = "git status";
+            gp = "git push ";
+            ga = "git add ";
+            gc = "git commit ";
+            gd = "git diff";
+            gdc = "git diff --cached";
+            gl = "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches";
         };
 
         oh-my-zsh = {
