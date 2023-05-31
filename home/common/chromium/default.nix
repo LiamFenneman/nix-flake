6f7779e467cf370237fece3e627685251b61908d
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-31 15:41:50 +1200

    chromium extensions

--- home/common/chromium/default.nix
+++ home/common/chromium/default.nix
@@ -5,10 +5,28 @@
         enable = true;
         extensions = [
             {
+                # uBlock Origin
                 id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
             }
             {
+                # Dark Reader
                 id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
+            }
+            { 
+                # RoboForm
+                id = "pnlccmojcmeohlpggmfnbbiapkmbliob";
+            }
+            {
+                # Enhancer for YouTube
+                id = "ponfpcnoihfmfllpaingbgckeeldkhle";
+            }
+            {
+                # Reddit Enhancement Suite
+                id = "kbmfpngjjgdllneeigpgjifpgocmfgmb";
+            }
+            {
+                # FrankerFaceZ
+                id = "fadndhdgpmmaapbmfcknlfgcflmmmieb";
             }
         ];
     };
