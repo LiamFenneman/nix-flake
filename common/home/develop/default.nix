c9fb36252bef798d0ca04d832942a128b317b607
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-06-13 21:37:27 +1200

    add rust src

--- common/home/develop/default.nix
+++ common/home/develop/default.nix
@@ -2,7 +2,7 @@
 
 let
   rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-      extensions = [ "rust-analyzer" ];
+      extensions = [ "rust-src" "rust-analyzer" ];
       targets = [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" ];
     });
 in
