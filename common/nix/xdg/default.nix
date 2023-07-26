cb3a8e36dcc628c6ce6ec55401c4ace3c205345b
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-07-26 17:04:11 +1200

    fix xdg issue with images

--- common/nix/xdg/default.nix
+++ common/nix/xdg/default.nix
@@ -5,5 +5,7 @@
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
   xdg.mime.defaultApplications = {
     "application/pdf" = "org.gnome.Evince.desktop";
+    "image/png" = "Ristretto.desktop";
+    "image/jpeg" = "Ristretto.desktop";
   };
 }
