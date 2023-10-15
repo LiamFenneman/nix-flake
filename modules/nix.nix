1ac6320059e56a41340d86a651f5ef27edac7829
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-10-15 20:39:48 +1300

    Add template registry and direnv to nix module

--- modules/nix.nix
+++ modules/nix.nix
@@ -8,17 +8,37 @@ in
 {
   options.custom.nix = {
     flakes.enable = mkEnableOption "Enable flakes" // { default = true; };
+    templates.enable = mkEnableOption "Enable templates" // { default = true; };
     gc.enable = mkEnableOption "Enable GC" // { default = true; };
+    direnv.enable = mkEnableOption "Enable direnv" // { default = true; };
     allowUnfree = mkEnableOption "Allow unfree packages" // { default = true; };
   };
 
   config = {
     nixpkgs.config.allowUnfree = cfg.allowUnfree;
 
+    programs.direnv = mkIf cfg.direnv.enable {
+      enable = true;
+      nix-direnv.enable = true;
+      silent = mkDefault true;
+    };
+
     nix = {
       settings = {
         experimental-features = mkIf cfg.flakes.enable [ "nix-command" "flakes" ];
         auto-optimise-store = mkDefault true;
+      };
+
+      registry.liam = mkIf cfg.templates.enable {
+        from = {
+          id = "liam";
+          type = "indirect";
+        };
+        to = {
+          owner = "LiamFenneman";
+          repo = "nix-flake-templates";
+          type = "github";
+        };
       };
 
       gc = mkIf cfg.gc.enable {
